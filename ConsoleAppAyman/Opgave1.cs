fb1be56c50688e0ef9e5f28ce08f9587c9a52324
ayman-a275 <ayman.dk01@gmail.com>
2025-02-25 09:53:17 +0100

    Create menu and solve assignment 1

---
+++ ConsoleAppAyman/Opgave1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace opgave1
+{
+    public class Opgave1
+    {
+        public static void Write()
+        {
+            int Age = 25;
+            float Height = 1.75f;
+            double Weight = 70.5;
+            char Initial = 'A';
+            string Name = "Alice";
+            bool isStudent = true;
+
+            Console.WriteLine($"Age: {Age}, Height: {Height}, Weight: {Weight}, Initial: {Initial}, Name: {Name}, Is Student: {isStudent}");
+        }
+    }
+}
--- ConsoleAppAyman/Program.cs
+++ ConsoleAppAyman/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using opgave1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(@"Menu
+1. Udskriv værdier på Alice.");
+
+
+        int userChoice = Convert.ToInt32(Console.ReadLine());
+
+        switch(userChoice){
+            case 1:
+                Opgave1.Write();
+                break;
+            default:
+                Console.WriteLine("Ugyldig værdi.");
+                break;
+        }
+
+        Console.ReadLine();
+    }
+}
