0c75a83de464a44eb39bfce0d556b53bf072b02c
Laura Hansen <laha71386@edu.ucl.dk>
2025-02-25 09:47:26 +0100

    he

---
+++ Laura/Opgave 1 Laura/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Opgave_1_Laura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, venner!");
+        }
+    }
+}
