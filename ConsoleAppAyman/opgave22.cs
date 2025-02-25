b0bbb0bfc9d7fd12237894a048a1abb2b2a2bd63
ayman-a275 <ayman.dk01@gmail.com>
2025-02-25 12:51:27 +0100

    Solve all assignments

---
+++ ConsoleAppAyman/opgave22.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace opgave22
+{
+    public class Opgave22
+    {
+        public static void Solution()
+        {
+            Console.WriteLine("Indtast et tal:");
+
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Indtast et andet tal:");
+
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+
+            ArealAfTrekant(ArealAfTrekant(userInput1, userInput2));
+            ArealAfRektangel(ArealAfRektangel(userInput1, userInput2));
+
+        }
+
+        public static string ArealAfTrekant(int a, int b)
+        {
+            return $"Arealet af en trekant er {0.5 * a * b}.";
+        }
+
+        public static void ArealAfTrekant(string besked)
+        {
+            Console.WriteLine(besked);
+        }
+
+        public static string ArealAfRektangel(int a, int b)
+        {
+            return $"Arealet af en rektangel {a * b}.";
+        }
+
+        public static void ArealAfRektangel(string besked)
+        {
+            Console.WriteLine(besked);
+        }
+    }
+}
