eb79beede1dd939fbb00e64c23a5ebd524dbe528
Freja <fbfl71372@edu.ucl.dk>
2025-02-25 10:36:36 +0100

    Opgave 1 løst

---
+++ Freja/ConsoleAppFreja/ConsoleAppFreja/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int age = 25;
+        float height = 1.75f;
+        double weight = 70.5;
+        char initial = 'A';
+        string name = "Alice";
+        bool isStudent = true;
+
+        Console.WriteLine($"Age: {age}, Height: {height}, Weight: {weight}, Initial: {initial}, Name: {name}, Is Student: {isStudent}");
+
+        Console.ReadKey();
+    }
+}
