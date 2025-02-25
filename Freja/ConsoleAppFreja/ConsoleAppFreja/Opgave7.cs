b539e6bdc410f7a32d7bc75ff4ecf139e04c3025
Freja <fbfl71372@edu.ucl.dk>
2025-02-25 14:24:04 +0100

    Oprettet opgave 7

---
+++ Freja/ConsoleAppFreja/ConsoleAppFreja/Opgave7.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace opgave7
+{
+    public class Opgave7
+    {
+        public static void TjekTal()
+        {
+            Console.WriteLine("Hejjj");
+        }
+    }
+}
