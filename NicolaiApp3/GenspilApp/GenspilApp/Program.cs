63803d4a924ab4ac2ae0d8a4ce495aa403c8e907
Alexander Skøtt Leismann <asle71355@edu.ucl.dk>
2025-03-24 10:36:36 +0100

    start

---
+++ NicolaiApp3/GenspilApp/GenspilApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenspilApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
